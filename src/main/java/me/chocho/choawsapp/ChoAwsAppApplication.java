9695f5736870d604d7c0d43d93ac825003f957de
DESKTOP-HN064OS\cafea <cafealpa@gmail.com>
2022-01-21 22:20:30 +0900

    Initial commit

---
+++ src/main/java/me/chocho/choawsapp/ChoAwsAppApplication.java
@@ -0,0 +1,13 @@
+package me.chocho.choawsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoAwsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChoAwsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/chocho/choawsapp/ChoAwsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.chocho.choawsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoAwsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
